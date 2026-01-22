db34712384e1b5baacd1faf48f42a8e315634fee
Prakash_T <prakaships@outlook.com>
2026-01-22 14:38:45 +0000

    code changes added

--- test.js
+++ test.js
@@ -1,1 +1,22 @@
-alert("Hello World!");
+alert("test Hello World!");
+
+function sumElements(m, f) {
+    let sum = 0;
+    for (let i of m) {
+        for (let j of f) {
+            if (j % i === 0) {
+                sum += i;
+                break;
+            }
+        }
+    }
+
+    return sum;
+}
+
+const elements = [2, 4, 3, 6];
+const multiplier = [3, 8, 9];
+
+console.log("array 1 =", ""+elements);
+console.log("array 2 =", ""+ multiplier);
+console.log(sumElements(elements, multiplier))
