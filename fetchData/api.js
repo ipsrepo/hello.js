5108f5394e3b5d348649000ce3319ea2607fc34e
Prakash_T <prakaships@outlook.com>
2026-02-03 15:04:21 +0000

    fetch updated

--- fetchData/api.js
+++ fetchData/api.js
@@ -2,5 +2,5 @@ document.getElementById('fetchData').addEventListener('click', async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
     const data = await res.json();
     console.log(data)
-    document.getElementById('data').innerText =  res.json();
+    document.getElementById('data').innerHTML =  `Task ID: ${data.id} <br> User ID: ${data.userId} <br> Title ${data.title} <br> isCompleted ${data.completed}`;
 })
